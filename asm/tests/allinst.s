6b1a977c47834a6180d11af2294751fbd908f383
wchen329 <wchen329@wisc.edu>
2019-11-25 18:23:29 -0600

    More work on the test

--- asm/tests/allinst.s
+++ asm/tests/allinst.s
@@ -10,37 +10,36 @@ and $5, $2, $4 #19444000
 andi $6, $2, 111 #2184006F
 or $7, $6, $5 #29CC5000
 ori $8, $6, 8055 #320C1F77
-xor $9, $7, $8 # 3AE48000
-sll $10, $1, $2
-srl $11, $1, $3
-sra $12, $1, $5
-lli $13, -1
-lui $14, -1
-sw $15, 100
-lw $16, 100
-lwo $17, 20($15)
-swo $18, 25($16)
-swo $19, 300($16)
+xor $9, $7, $8 #3A4E8000
+sll $10, $1, $2 #42822000
+srl $11, $1, $3 #4AC23000
+sra $12, $1, $5 #53025000
+lli $13, -1 #5B40FFFF
+lui $14, -1 #6380FFFF
+sw $15, 100 #73C00064
+lw $16, 100 #6C000064
+lwo $17, 20($15) #7C5E0014
+swo $18, 25($16) #84A00019
 
 # Branch Instructions
-beq BEGIN
-ble BEGIN
-bge BEGIN
-bne BEGIN
-bgt AFTER
-blt AFTER
-bov AFTER
-b BEGIN
+beq BEGIN # 8900FFEE
+ble BEGIN # 8D00FFED
+bge BEGIN # 8C00FFEC
+bne BEGIN # 8800FFEB
+bgt AFTER # 8A00000B
+blt AFTER # 8B00000A
+bov AFTER # 8E000009
+b BEGIN # 8F00FFE7
 
 # Branch "and Link" Instructions
-bleq BEGIN
-blle BEGIN
-blge AFTER
-blne AFTER
-blgt AFTER
-bllt AFTER
-blov BEGIN
-bl BEGIN
+bleq BEGIN # 9100FFE6
+blle BEGIN # 9500FFE5
+blge AFTER # 94000005
+blne AFTER # 90000004
+blgt AFTER # 92000003
+bllt AFTER # 93000002
+blov BEGIN # 9600FFE0
+bl BEGIN # 9700FFDF
 
 AFTER:
 ret
--- asm/tests/testpkg.cpp
+++ asm/tests/testpkg.cpp
@@ -6,9 +6,9 @@
 int correct_count = 0;
 int count = 0;
 
-void mark_incorrect(const std::string& actual, const std::string& expected)
+void mark_incorrect(int where, const std::string& actual, const std::string& expected)
 {
-	fprintf(stdout, "Incorrect sequence detected:\n");
+	fprintf(stdout, "Incorrect sequence detected at line %d:\n", where);
 	fprintf(stdout, "\tExpected:%s\n\tActual:%s\n", expected.c_str(), actual.c_str());
 	count++;
 }
@@ -21,9 +21,8 @@ int main(int argc, char** argv)
 {
 	std::map<int, std::string> num_to_output;
 	fprintf(stdout, "ASSEMBLER GENERAL INSTRUCTION TEST\n");
-	fprintf(stdout, "---\n");
 
-	// Set reference assembly.
+	// Set reference hex values.
 	num_to_output[0] = "08860032";
 	num_to_output[1] = "00443000";
 	num_to_output[2] = "11047000";
@@ -31,20 +30,40 @@ int main(int argc, char** argv)
 	num_to_output[4] = "2184006F";
 	num_to_output[5] = "29CC5000";
 	num_to_output[6] = "320C1F77";
-	num_to_output[7] = "3AE48000";
+	num_to_output[7] = "3A4E8000";
 	num_to_output[8] = "42822000";
 	num_to_output[9] = "4AC23000";
 	num_to_output[10] = "53025000";
 	num_to_output[11] = "5B40FFFF";
-	num_to_output[12] = "";
-	num_to_output[13] = "";
-	num_to_output[14] = "";
-	num_to_output[15] = "";
-	num_to_output[16] = "";
-	num_to_output[17] = "";
-	num_to_output[18] = "";
-	num_to_output[19] = "";
-	num_to_output[20] = "";
+	num_to_output[12] = "6380FFFF";
+	num_to_output[13] = "73C00064";
+	num_to_output[14] = "6C000064";
+	num_to_output[15] = "7C5E0014";
+	num_to_output[16] = "84A00019";
+	num_to_output[17] = "8900FFEE";
+	num_to_output[18] = "8D00FFED";
+	num_to_output[19] = "8C00FFEC";
+	num_to_output[20] = "8800FFEB";
+	num_to_output[21] = "8A00000B";
+	num_to_output[22] = "8B00000A";
+	num_to_output[23] = "8E000009";
+	num_to_output[24] = "8F00FFE7";
+	num_to_output[25] = "9100FFE6";
+	num_to_output[26] = "9500FFE5";
+	num_to_output[27] = "94000005";
+	num_to_output[28] = "90000004";
+	num_to_output[29] = "92000003";
+	num_to_output[30] = "93000002";
+	num_to_output[31] = "9600FFE0";
+	num_to_output[32] = "9700FFDF";
+	num_to_output[33] = "";
+	num_to_output[34] = "";
+	num_to_output[35] = "";
+	num_to_output[36] = "";
+	num_to_output[37] = "";
+	num_to_output[38] = "";
+	num_to_output[39] = "";
+	num_to_output[40] = "";
 
 	// Open reference test file
 	FILE* f = fopen("test.hl", "r");
@@ -71,7 +90,7 @@ int main(int argc, char** argv)
 		std::string actual = std::string(buf);
 		if(actual != num_to_output[i])
 		{
-			mark_incorrect(actual, num_to_output[i]);
+			mark_incorrect(i, actual, num_to_output[i]);
 		}
 		else correct_count++;
 		i++;

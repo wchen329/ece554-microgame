c2cbfcc043633bd17fc0aa9bdcf84f45f94b80d1
sw8lde <sw8lde@gmail.com>
2019-12-03 16:20:29 -0600

    created sprite test asm and mif files

--- v/sprite/sprite_ds.s
+++ v/sprite/sprite_ds.s
@@ -1,22 +1,8 @@
-# draw inside border
-# draw blue top left
-lli $31, hFF
-lui $31, 0
-lli $1, 8
-lli $2, 8
-wfb $1, $2
-# draw green top right
-lli $31, hFF00
-lli $1, 248
-wfb $1, $2
-# draw blue bot right
-lli $31, hFF00
-lli $2, 248
-wfb $1, $2
-# draw white bot left
-lli $31, hFFFF
-lui $31, hFF
-lli $1, 8
-wfb $1, $2
+# draw sprites inside border
+# draw rotated 0
+ls %0, 0, SPRITE
+lli $1, 20
+lli $2, 10
+ds %0, $1, $2
 
 dfb

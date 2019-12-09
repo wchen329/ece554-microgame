d5c8c6ea88867c3c41d781a78d4efbf55468b675
sw8lde <sw8lde@gmail.com>
2019-12-09 15:02:25 -0600

    organize sprite demos

--- v/sprite/asm/sprite_draw.s
+++ v/sprite/asm/sprite_draw.s
@@ -45,24 +45,25 @@ dfb
 # draw sprites inside border
 # draw rotated 0
 ls %0, 0, SPRITE
+ls %1, 0, SQ_SPRITE
 lli $1, 20
 lli $2, 10
-ds %0, $1, $2
+ds %1, $1, $2
 # draw rotated 1
-rs %0, 1
+rs %1, 1
 lli $1, 30
 lli $2, 20
-ds %0, $1, $2
+ds %1, $1, $2
 # draw rotated 2
-rs %0, 2
+rs %1, 2
 lli $1, 20
 lli $2, 30
-ds %0, $1, $2
+ds %1, $1, $2
 # draw rotated 3
-rs %0, 3
+rs %1, 3
 lli $1, 10
 lli $2, 20
-ds %0, $1, $2
+ds %1, $1, $2
 
 dfb
 
@@ -74,13 +75,13 @@ lli $2, 0
 ds %0, $1, $2
 # draw rotated 1
 rs %0, 1
-lli $1, 255
+lli $1, 248
 lli $2, 124
 ds %0, $1, $2
 # draw rotated 2
 rs %0, 2
 lli $1, 124
-lli $2, 255
+lli $2, 248
 ds %0, $1, $2
 # draw rotated 3
 rs %0, 3
@@ -90,38 +91,9 @@ ds %0, $1, $2
 
 dfb
 
-# clear sprites inside screen
-lli $1, 20
-lli $2, 10
 cs $1, $2
-# clear rotated 1
-lli $1, 30
-lli $2, 20
-cs $1, $2
-# clear rotated 2
-lli $1, 20
-lli $2, 30
-cs $1, $2
-# clear rotated 3
-lli $1, 10
-lli $2, 20
-cs $1, $2
-
 dfb
 
-# clear sprites at edge of screen
-lli $1, 124
-lli $2, 0
-cs $1, $2
-# clear rotated 1
-lli $1, 255
-lli $2, 124
-cs $1, $2
-# clear rotated 2
-lli $1, 124
-lli $2, 255
-cs $1, $2
-# clear rotated 3
-lli $1, 0
-lli $2, 124
-cs $1, $2
+LOOP:
+andi $1, $1, 0
+beq LOOP

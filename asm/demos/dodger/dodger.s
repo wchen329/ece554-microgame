528abe36cf1c91a1dc48f8cf1aed0ada8858535d
sw8lde <sw8lde@gmail.com>
2019-12-12 15:50:21 -0600

    updated dodger

--- asm/demos/dodger/dodger.s
+++ asm/demos/dodger/dodger.s
@@ -8,16 +8,34 @@
 # $4  - rock y
 # $5  - rock rot
 # $6  - input
+# $7  - rock speed
 # $10+ - gp/test
 # $28 - time curr
 # $29 - time end
 
-# set up
-ls %0, 0, SHIP
-ls %1, 0, ROCK
-addi $1, $zero, 128
-addi $2, $zero, 240
-bl GEN_ROCK
+# intro screen
+ls %0, 0, d
+ls %1, 0, o
+ls %2, 0, g
+ls %3, 0, e
+ls %4, 0, r
+
+lli $10, 100
+lli $11, 120
+# DODGER
+ds %0, $10, $11
+addi $10, $10, 9
+ds %1, $10, $11
+addi $10, $10, 9
+ds %0, $10, $11
+addi $10, $10, 9
+ds %2, $10, $11
+addi $10, $10, 9
+ds %3, $10, $11
+addi $10, $10, 9
+ds %4, $10, $11
+
+dfb
 
 # wait for input
 WAIT_INPUT:
@@ -26,7 +44,7 @@ WAIT_INPUT:
 
 	lk $1
 	addi $1, $1, 0
-	bne GAME
+	bne SET_UP
 
 	WAIT_I:
 	 	tim $28
@@ -43,6 +61,12 @@ SET_RIGHT:
 	ret
 
 GEN_ROCK:
+	# random speed
+	# allow speeds of 2, 3, 4, 5
+	r $7
+	andi $7, $7, 3
+	addi $7, $7, 2
+	# random loc
 	r $3
 	andi $3, $3, hFF
 	andi $4, $4, 0
@@ -92,6 +116,30 @@ DETECT_COL:
 
 	ret
 
+SET_UP:
+	# clear title
+	lli $10, 100
+	lli $11, 120
+	cs $10, $11
+	addi $10, $10, 9
+	cs $10, $11
+	addi $10, $10, 9
+	cs $10, $11
+	addi $10, $10, 9
+	cs $10, $11
+	addi $10, $10, 9
+	cs $10, $11
+	addi $10, $10, 9
+	cs $10, $11
+
+	# set up
+	ls %0, 0, SHIP
+	ls %1, 0, ROCK
+	addi $1, $zero, 128
+	addi $2, $zero, 240
+	addi $7, $zero, 2
+	bl GEN_ROCK
+
 GAME:
 	# get time so we know when this frame should end
 	tim $29
@@ -99,9 +147,8 @@ GAME:
 	addi $29, $29, h1
 
 	# check coll
-	# addi $10, $4, -230
-	# blgt DETECT_COL
-	bl DETECT_COL
+	addi $10, $4, -232
+	blgt DETECT_COL
 
 	cs $1, $2
 	cs $3, $4
@@ -119,24 +166,25 @@ GAME:
 	bne END
 
 	# move rock
-	addi $4, $4, 2
+	add $4, $4, $7
 	addi $10, $4, -250
 	blgt GEN_ROCK
 
 	# rotate rock
+	# use $5[2:1] so it rotates slower
 	addi $5, $5, 1
-	andi $5, $5, 3
+	andi $5, $5, 7
 
 	addi $10, $5, 0
 	bleq ROT_0
 
-	addi $10, $5, -1
+	addi $10, $5, -2
 	bleq ROT_1
 
-	addi $10, $5, -2
+	addi $10, $5, -4
 	bleq ROT_2
 
-	addi $10, $5, -3
+	addi $10, $5, -6
 	bleq ROT_3
 
 	# draw new positions
@@ -157,14 +205,13 @@ GAME:
 	b GAME
 
 END:
-ls %0, 0, GO_0
-ls %1, 0, GO_1
-ls %2, 0, GO_2
-ls %3, 0, GO_3
-ls %4, 0, GO_4
-ls %5, 0, GO_5
-ls %6, 0, GO_6
-ls %7, 0, GO_7
+ls %0, 0, g
+ls %1, 0, a
+ls %2, 0, m
+ls %3, 0, e
+ls %4, 0, o
+ls %5, 0, v
+ls %7, 0, r
 
 andi $10, $10, 0
 andi $11, $11, 0
